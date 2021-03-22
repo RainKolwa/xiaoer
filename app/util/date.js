335cc68404515912bd11b64671d4752fd7bd0ce4
rainkolwa <rainkolwa@gmail.com>
2021-03-22 13:07:51 +0800

    fix: timezone

--- app/util/date.js
+++ app/util/date.js
@@ -2,12 +2,12 @@
 const dayjs = require('dayjs');
 const utc = require('dayjs/plugin/utc'); // dependent on utc plugin
 const timezone = require('dayjs/plugin/timezone');
+const TIMEZONE = 'Asia/Shanghai';
 dayjs.extend(utc);
 dayjs.extend(timezone);
-dayjs.tz.setDefault('Asia/Shanghai');
 
 module.exports = {
   format: (date, format = 'YYYY-MM-DD') => {
-    return dayjs(date).format(format);
+    return dayjs(date).tz(TIMEZONE).format(format);
   },
 };
